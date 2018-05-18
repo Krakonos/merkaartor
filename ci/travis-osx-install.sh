d037da06692a23b7366960ce0f7e2d31edb71d82
Ladislav Láska <krakonos@krakonos.org>
2018-05-19 00:49:53 +0200

    I'm out of ideas, let's just ignore the brew command outputs.

--- ci/travis-osx-install.sh
+++ ci/travis-osx-install.sh
@@ -19,12 +19,13 @@ brew uninstall --ignore-dependencies --force gdal
 # To get GDAL 2.0: http://gis.stackexchange.com/questions/155403/install-gdal-2-0-on-a-macosx
 #brew install gdal --HEAD
 
-brew unlink python
-
 # Gdal and proj are already up to date, libspatialite is probably required by
 # one of them and that screws macdeployqt later on...
-brew install --overwrite Dylibbundler gdal $QT_PKG
-brew link --force $QT_PKG
+#
+# Moreover, we will ignore the results as homebrew fails as it wishes.  The
+# build will fail later anyway if something doesn't install properly.
+brew install Dylibbundler gdal $QT_PKG || echo "Install might have failed. Ignoring"
+brew link --force $QT_PKG || echo "Link might have failed. Ignoring."
 
 find /usr/local -name "qmake*"
 find /usr/local -name "proj_api.h"
