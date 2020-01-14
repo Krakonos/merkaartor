ceeb9a29a5f10c9980bfe1d89400ca2aa3824001
Ladislav Láska <krakonos@krakonos.org>
2020-01-14 11:27:55 +0100

    Correct qt_repo usage.

--- ci/travis-linux-install.sh
+++ ci/travis-linux-install.sh
@@ -8,7 +8,7 @@ sudo echo "deb http://archive.ubuntu.com/ubuntu ${TRAVIS_DIST} main universe res
 if [ "${TRAVIS_DIST}" == trusty ]; then
 	sudo apt-add-repository -y ppa:ubuntu-sdk-team/ppa
 fi
-sudo apt-add-repository -y ppa:beineri/${QT_REPO}
+sudo apt-add-repository -y ${QT_REPO}
 sudo apt-get update -qq
 sudo apt-get -y install gdb libgdal-dev libproj-dev qt${QT_PREFIX}-meta-minimal qt${QT_PREFIX}svg build-essential libgl1-mesa-dev
 qtchooser -list-versions
