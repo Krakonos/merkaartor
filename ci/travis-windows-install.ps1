a23e3a8da0085fb91b1af780824e1ffd371f5ba4
Ladislav Láska <krakonos@krakonos.org>
2020-01-14 12:05:08 +0100

    Attempt at adding vcpkg and msvc build.

--- ci/travis-windows-install.ps1
+++ ci/travis-windows-install.ps1
@@ -2,6 +2,8 @@
 powershell Set-MpPreference -DisableRealTimeMonitoring 1
 powershell Set-MpPreference -DisableBehaviorMonitoring 1
 
+if $( $ENV:BUILD_DRIVER -like 'MSYS2*' ) {
+
 gdr
 
 choco list -localonly
@@ -15,6 +17,14 @@ taskkill -IM "gpg-agent.exe" -F
 gdr
 
 c:\tools\msys64\usr\bin\bash.exe -l -c "pacman --noconfirm --needed -S base-devel msys/git mingw64/mingw-w64-x86_64-gcc mingw64/mingw-w64-x86_64-qt5 mingw64/mingw-w64-x86_64-gdal mingw64/mingw-w64-x86_64-proj mingw64/mingw-w64-x86_64-openjpeg2 mingw64/mingw-w64-x86_64-json-c mingw64/mingw-w64-x86_64-cmake mingw64/mingw-w64-x86_64-exiv2 mingw64/mingw-w64-x86_64-nsis"
+
+}
+
+if $( $ENV:BUILD_DRIVER -like 'MSVC*' ) {
+
+vcpkg install proj4 gdal exiv2 qt5
+
+}
 
 ## Install Qt first, as it requires quite a bit of space
 #c:\tools\msys64\usr\bin\bash.exe -l -c "pacman --noconfirm --needed -S mingw64/mingw-w64-x86_64-qt5"
